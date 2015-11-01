65a00fa54133d578151357fb6014373fef1c0d54
slimko <slimko2008@yandex.ru>
2015-11-01 19:40:34 +0300

    готовая домашка

--- dz6/dz6.php
+++ dz6/dz6.php
@@ -1,142 +1,147 @@
 ﻿<?php
 session_start();//стартуем сессию
-$citys = array('641780'=>'Новосибирск','641490'=>'Барабинск','641510'=>'Бердск');
-$transport = array('9'=>'Автомобили с пробегом','109'=>'Новые автомобили','14'=>'Мотоциклы и мототехника','81'=>'Грузовики и спецтехника','11'=>'Водный транспорт','10'=>'Запчасти и аксессуары');
+//подключаем массив с данными
+require_once 'array.php';
 
-//функция присваивания id объявления в сессии
-function check_session(){
-    if ( ! isset( $_SESSION['counter'] )) 
-    { 
-        $_SESSION['counter'] = 0; 
-    } 
-    else 
-    { 
-        $_SESSION['counter']++; 
-    }
-    return $_SESSION['counter'];
-}
-
-//функция добавления/изменения данных в массиве session
-function add_ads($var,$i=NULL){
-    if($i==null){
-        $i = check_session(); //если объявление не изменяем, то получаем id нового объявления  
-    }else{
-        unset($_SESSION['ads'][$i]); //очищаем переменную для перезаписи, используется для того, чтобы было понятно изменили ли значение переменной - "Я не хочу получать вопросы по объявлению по e-mail"
-    }
-    foreach ($var as $key => $value) {
-        $_SESSION['ads'][$i][$key]=$value; //записываем значение в сессию
-    }
-
-}
-//функция выводит блок данных из session: название|цену|удалить|
-function show_ads($data){
-    //array_multisort($data,SORT_DESC); //сортируем массив по порядку
-    foreach ($data as $k => $v) {
-            echo $k.'. <a href="?id='.$k.'">'.$v['title'].'</a> | '.$v['price'].' руб. | <a href="?del='.$k.'">удалить</a><br>';
-    }
-}
-
-//проверяем массив пост
-if($_POST!=NULL){
-    if(isset($_SESSION['form'])){ //если существует этот массив в сессии, значит редактируем объявление
-        $i=$_SESSION['form']['id'];//передаем в переменную id изменяемого объявления
-        unset($_SESSION['form']); //удаляем
-        add_ads($_POST,$i); //передаем массив POST из формы с id объявления
-    }else{
-        add_ads($_POST); //передаем массив POST из формы для добавления нового объявления
-    }
-}
-
-//проверяем наичие гета и сессии
-if(isset($_GET,$_SESSION['ads'])){
-    foreach ($_GET as $k => $v){
-        switch ($k) {
-            case 'id':
-                if(is_numeric($v) and isset($_SESSION['ads']["$v"])){
-                    $_SESSION['form']['id']=$v; //запишем id новости в массив для последующего редактирования из формы
-                    $var=$_SESSION['ads']["$v"]; //распарсим массив для редактирования в форме
-                    foreach ($var as $key => $value) {
-                        $_SESSION['form'][$key]=$value;
-                    }
-                }
-                else{
-                    echo "<h2>Такой записи не существует</h2>";
-                }    
-            break;
-            case 'del':
-                unset($_SESSION['ads']["$v"]);
-                break;
-            default:
-                header("HTTP/1.1 404 Not Found");
-                header('Location: /404.html');
-                exit();     
+//функция выводит данные в select
+function show_select($array){
+    foreach ($array as $k => $v){
+        if(is_array($v)){
+            echo '<optgroup label="'.$k.'">';
+            show_select($v);
+            echo '</optgroup>';
+        }   
+        else{
+            if($_SESSION['ads'][$_GET['id']]['city']==$k or $_SESSION['ads'][$_GET['id']]['cat']==$k){$selected='selected';}else{$selected='';}
+            //$selected = ($k==$_SESSION['ads'][$_GET['id']][$k]) ? 'selected' : ''; //если мы передали в функцию город который нужно выставить в списке то мы ставим специальную метку в селектор
+            echo '<option value="'.$k.'"'.$selected.'>'.$v.'</option>';
+            }
         }
-    }  
 }
-else{
-    unset($_SESSION['form']); //если массив гет пустой - очищаем данные из формы в сессии
+//проверяем массив пост и добавляем в сессию
+if($_POST){
+    if(isset($_POST['id'])){
+        $i=$_POST['id'];
+        $_SESSION['ads'][$i]=$_POST;
+    }   
+    else{
+        $_SESSION['ads'][]=$_POST; //записываем значение в сессию
+    }
+}
+//проверяем массив get на запрос про удаление
+if(isset($_GET['del'])){
+    if(is_numeric($_GET['del']) and $_GET['del']<=count($_SESSION['ads'])){
+        $i=$_GET['del'];
+        unset($_SESSION['ads'][$i]);
+    }
+    else{
+        echo 'Нет данных для удаления';
+    } 
 }
 
-?>
+//функция выводит блок данных из session: название|цену|удалить|
+function show_ads(){
+    echo '<p>Все объявления:</p><ul>';
+    if(!empty($_SESSION['ads'])){
+        foreach ($_SESSION['ads'] as $k => $v) {
+        echo '<li>'.$k.'. <a href="?id='.$k.'">'.$v['title'].'</a> | '.$v['price'].' руб. | <a href="?del='.$k.'">удалить</a></li>';
+    }
+    echo '</ul>';
+    }else{
+        echo 'Вы еще не добавили ни одного объявления';
+    }
+}
+?>   
+<html>
+<head>
+    <style type="text/css">
+        .contact_form {
+            border: 1px solid gray;
+            width: 550px;
+        }
+        .contact_form .input_name_label {
+            width:160px;
+            display:inline-block;
+            margin:5px;
+            vertical-align: top;
+        }
+        .contact_form .input_text {
+            padding:2px;
+            margin: 5px;
+            width: 300px;
+        }
+        .contact_form textarea {padding:8px; margin: 5px;width:300px;}
+        .contact_form button, .contact_form .input_radio, {margin-right: 40px; padding: 5px;}
+        .contact_form .allow_mails{
+            margin:10px;
+        }
+        .wrapper{
+            width:300px;
+            display:inline-block;
+            padding:2px;
+            margin: 5px;
+            margin-left: 180px;    
+        }
+        li{
+            list-style-type: none;
+        }
+    </style>
+</head>
+<body>
 <!--форма для добавления -->
-<form action="dz6.php" method="post"> 
-    <label for="private">Частное лицо</label> 
-        <input type="radio" value="1" <?php if(isset($_SESSION['form']['private']) && $_SESSION['form']['private']=="1")echo 'checked=""';?>name="private">
-    <label for="company">Компания</label>
-        <input type="radio" value="0" <?php if(isset($_SESSION['form']['private']) && $_SESSION['form']['private']=="0")echo 'checked=""';?>name="private"><br>
-    <label for="name">Ваше имя</label>
-        <input type="text" maxlength="40" name="name" id="name" value='<?php if(isset($_SESSION['form'])){echo $_SESSION['form']['name'];}?>'><br>
-    <label for="email">Электронная почта</label>
-        <input type="text" name="email" value='<?php if(isset($_SESSION['form'])){echo $_SESSION['form']['email'];}?>'><br>
-    <label for="allow_mails"> 
-        <input type="checkbox" name="allow_mails" 
-        <?php if(isset($_SESSION['form']) and isset($_SESSION['form']['allow_mails'])=='on'){echo 'checked';} else {echo '';}?>>Я не хочу получать вопросы по объявлению по e-mail</label><br>
-    <label for="fld_phone">Номер телефона</label> 
-        <input type="text" name="phone" id="fld_phone" value='<?php if(isset($_SESSION['form'])){echo $_SESSION['form']['phone'];}?>'><br>
-    <label for="region" >Город</label> 
-        <select title="Выберите Ваш город" name="region"> 
-            <option value="">-- Выберите город --</option>
+<form action="dz6.php" method="post" class="contact_form"> 
+   <?php if(isset($_GET['id']) and is_numeric($_GET['id']) and $_GET['id']<=count($_SESSION['ads'])){echo '<input type="hidden"  name="id"  value="'.$_GET['id'].'">';}?>
+   <div class="wrapper">
+         <label for="private" >Частное лицо</label> 
+            <input type="radio" class="input_radio" 
+            <?php if(isset($_GET['id']) and !empty($_SESSION['ads'][$_GET['id']]['private']) and $_SESSION['ads'][$_GET['id']]['private']=='1')
+            {
+                echo 'value="1" checked ';
+                }
+                else{ echo 'value="1"';
+            }?> name="private">
+        <label for="company">Компания</label>
+            <input type="radio" class="input_radio" 
+            <?php if(isset($_GET['id']) and !empty($_SESSION['ads'][$_GET['id']]['private']) and $_SESSION['ads'][$_GET['id']]['private']=='0'){
+                echo 'value="0" checked ';
+            }
+            elseif(!isset($_GET['id'])){ echo 'value="0" checked';
+            }
+            else{ echo 'value="0"';
+            }
+            ?> name="private">
+    </div>
+    <br>
+    <label for="name" class="input_name_label">Ваше имя</label>
+        <input type="text" class="input_text" maxlength="40" name="name" id="name" <?php if(isset($_GET['id']) and !empty($_SESSION['ads'][$_GET['id']]['name'])){echo 'value="'.$_SESSION['ads'][$_GET['id']]['name'].'"';}?>><br>
+    <label for="email" class="input_name_label">Электронная почта</label>
+        <input type="text" name="email" class="input_text" <?php if(isset($_GET['id']) and !empty($_SESSION['ads'][$_GET['id']]['email'])){echo 'value="'.$_SESSION['ads'][$_GET['id']]['email'].'"';}?>><br>
+    <label for="allow_mails" > 
+        <input type="checkbox" name="allow_mails" class="allow_mails" <?php if(isset($_GET['id']) and !empty($_SESSION['ads'][$_GET['id']]['allow_mails'])){echo 'checked';}?>>Я не хочу получать вопросы по объявлению по e-mail</label><br>
+    <label for="fld_phone" class="input_name_label">Номер телефона</label> 
+        <input type="text" class="input_text" name="phone" id="fld_phone" <?php if(isset($_GET['id']) and !empty($_SESSION['ads'][$_GET['id']]['phone'])){echo 'value="'.$_SESSION['ads'][$_GET['id']]['phone'].'"';}?>><br>
+    <label for="region" class="input_name_label" >Город</label> 
+        <select class="input_text" title="Выберите Ваш город" name="city"> 
+            <option value="0">-- Выберите город --</option>
             <option disabled="disabled">-- Города --</option>
-            <?php
-            foreach($citys as $number=>$city){
-                $selected = ($number==$_SESSION['form']['region']) ? 'selected=""' : ''; //если мы передали в функцию город который нужно выставить в списке то мы ставим специальную метку в селектор
-                        echo '<option '.$selected.' value="'.$number.'">'.$city.'</option>';
-                    }
-            ?>   
+            <?php show_select($city); ?>
         </select>
         <br>
-    <label for="fld_category_id" class="form-label">Категория</label>
-        <select title="Выберите категорию объявления" name="transport" class="form-input-select">
+    <label for="fld_category_id"  class="input_name_label">Категория</label>
+        <select title="Выберите категорию объявления" name="cat" class="input_text">
             <option value="">-- Выберите категорию --</option>
-            <optgroup label="Транспорт">
-
-                <?php
-                foreach($transport as $number=>$bibi){
-                $selected = ($number==$_SESSION['form']['transport']) ? 'selected=""' : ''; //если мы передали в функцию город который нужно выставить в списке то мы ставим специальную метку в селектор
-                        echo '<option '.$selected.' value="'.$number.'">'.$bibi.'</option>';
-                    }
-                ?>   
-            </optgroup>
+            <?php show_select($cat) ?>
         </select>
     <br>
-    <label for="fld_title">Название объявления</label>
-        <input type="text" maxlength="50" name="title" value='<?php if(isset($_SESSION['form'])){echo $_SESSION['form']['title'];}?>'><br>
-    <label for="fld_description" >Описание объявления</label>
-        <textarea maxlength="3000" name="description"><?php if(isset($_SESSION['form'])){echo $_SESSION['form']['description'];}?></textarea><br>
-    <label for="fld_price">Цена</label>
-    <input type="text" maxlength="9" name="price" value='<?php if(isset($_SESSION['form'])){echo $_SESSION['form']['price'];}else{echo 0;}?>'>
+    <label for="fld_title" class="input_name_label">Название объявления</label>
+        <input type="text" class="input_text" maxlength="50" name="title" <?php if(isset($_GET['id']) and !empty($_SESSION['ads'][$_GET['id']]['title'])){echo 'value="'.$_SESSION['ads'][$_GET['id']]['title'].'"';}?>><br>
+    <label for="fld_description"  class="input_name_label">Описание объявления</label>
+        <textarea maxlength="3000" name="description"><?php if(isset($_GET['id']) and !empty($_SESSION['ads'][$_GET['id']]['description'])){echo $_SESSION['ads'][$_GET['id']]['description'];}?></textarea><br>
+    <label for="fld_price"  class="input_name_label">Цена</label>
+    <input type="text" class="input_text" maxlength="9" name="price" <?php if(isset($_GET['id']) and !empty($_SESSION['ads'][$_GET['id']]['price'])){echo 'value="'.$_SESSION['ads'][$_GET['id']]['price'].'"';}else{echo '0';}?>>
         &nbsp;руб.<br>
-    <button type="submit">Отправить</button>
+    <div class="wrapper"><button type="submit">Отправить</button><button type="reset">Очистить</button></div>
 </form>
-
-<?php
-
-
-//проверяем массив сессии и выводим объявления
-if(isset($_SESSION['ads'])){
-    show_ads($_SESSION['ads']);
-}else{
-    echo 'Вы еще не добавили ни одного объявления';
-}
-
-?>
+<?php show_ads(); //выводим объявления?>
+</body>
+</html>
