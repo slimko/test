6dc9951df4e7743ea779ebb70e96f99aeb3faea2
slimko <slimko2008@yandex.ru>
2016-01-08 02:53:53 +0300

    дз 9 с pdo

---
+++ dz9/dz9-pdo/smarty/templates/index.tpl
@@ -0,0 +1,4 @@
+{config_load file='dz8.conf'}
+{include file='header.tpl'}
+{include file='body.tpl'}
+{include file='footer.tpl'}
---
+++ dz9/dz9-pdo/smarty/templates_c/%%45^45E^45E480CD%%index.tpl.php
@@ -0,0 +1,21 @@
+<?php /* Smarty version 2.6.28, created on 2015-12-13 15:03:41
+         compiled from index.tpl */ ?>
+<?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
+smarty_core_load_plugins(array('plugins' => array(array('function', 'config_load', 'index.tpl', 1, false),)), $this); ?>
+<?php echo smarty_function_config_load(array('file' => 'dz8.conf'), $this);?>
+
+<?php $_smarty_tpl_vars = $this->_tpl_vars;
+$this->_smarty_include(array('smarty_include_tpl_file' => 'header.tpl', 'smarty_include_vars' => array()));
+$this->_tpl_vars = $_smarty_tpl_vars;
+unset($_smarty_tpl_vars);
+ ?>
+<?php $_smarty_tpl_vars = $this->_tpl_vars;
+$this->_smarty_include(array('smarty_include_tpl_file' => 'body.tpl', 'smarty_include_vars' => array()));
+$this->_tpl_vars = $_smarty_tpl_vars;
+unset($_smarty_tpl_vars);
+ ?>
+<?php $_smarty_tpl_vars = $this->_tpl_vars;
+$this->_smarty_include(array('smarty_include_tpl_file' => 'footer.tpl', 'smarty_include_vars' => array()));
+$this->_tpl_vars = $_smarty_tpl_vars;
+unset($_smarty_tpl_vars);
+ ?>
