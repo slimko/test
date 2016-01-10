d39356c438adff726ddd5f68661c06bf935c93eb
slimko <slimko2008@yandex.ru>
2016-01-10 22:41:14 +0300

    дз10

---
+++ dz10/smarty/templates_c/%%3C^3CB^3CB46C1C%%header_mobile.tpl.php
@@ -0,0 +1,9 @@
+<?php /* Smarty version 2.6.28, created on 2014-12-10 13:52:58
+         compiled from header_mobile.tpl */ ?>
+<HTML>
+<HEAD>
+<TITLE>MOBILE: <?php echo $this->_tpl_vars['title']; ?>
+ / <?php echo $this->_tpl_vars['var1']; ?>
+</TITLE>
+</HEAD>
+<BODY bgcolor="#ffffff">
