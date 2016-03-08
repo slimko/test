a3ea5f9629257a7cb6e7126079a8f4901afdca82
Slimko <slimko2008@yandex.ru>
2016-03-08 04:03:49 +0300

    dz

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -2,48 +2,39 @@
 //основной класс по работе с объявлениями (запись, удаление, редактирование)
 class IndexController {
 	private $postAds = NULL; //массив пост
-	private $ViewModels; //переменная модели
+	private $Models; //переменная модели объявлений
+	private $fc; //FrontController
 
 	public function __construct($ads_post){
-		if($ads_post!==null)$this->postAds=$ads_post; //если есть пост пишем в свойство $postAds
-		$this->ViewModels = new Model(); //инициализируем модель
+		if($ads_post!==null)$this->postAds=$ads_post;    //если есть $_POST пишем в свойство $postAds
+		$this->Models = new Ads($this->postAds);		//инициализируем модель
+		$this->fc = FrontController::getInstance();    //инициализируем фронт контроллер
 	}
 
-	//метод срабатывающий при первом запуске программы или при отсутствии других параметров
+	/** метод срабатывающий при первом запуске программы или при отсутствии других параметров */
 	function indexAction(){
-		$main = StorageController::getInstance(); //подключаемся к хранилищу
-		$ads = $main->getAdsFromBD(); //получаем данные объявлений из хранилища
-		$fc=FrontController::getInstance();
-		$fc->setBody($this->ViewModels->render($ads)); //выводим пользователю результат (рендерим страницу)
+		$ads = $this->Models->getAds(); //получаем объявления
+		$this->fc->setBody($this->Models->render($ads)); //выводим пользователю результат (рендерим страницу)
 	}
 
-	//метод обрабатывающий отправку формы
+	/** метод обрабатывающий отправку формы  */
 	function postAction(){
-		$ads = new AdsController($this->postAds); //вызываем класс по работе с объявлениями и передаем ему наш ПОСТ массив
-			$ads->postAds(); //добавляем нашу запись в базу данных
+		$this->Models->postAds(); //добавляем нашу запись в базу данных
 		$this->indexAction(); //продолжаем отображение данных и страницы
 	}
 
-	//метод работающий при гет запросе - запросить запись на редактирование
+	/** метод работающий при гет запросе - запросить запись на редактирование  */
 	function getAction(){
-		$main = StorageController::getInstance(); //подключаемся к хранилищу
-		$ads = $main->getAdsFromBD(); //получаем данные объявлений из хранилища
-		$fc=FrontController::getInstance();
-		$getId = $fc->getParams();//получаем id
-		$fc->setBody($this->ViewModels->render($ads,$getId['id'])); //выводим пользователю результат (рендерим страницу)
+		$ads = $this->Models->getAds(); //получем объявления
+		$id = $this->fc->getParams();//получаем id редактируемой записи
+		$this->fc->setBody($this->Models->render($ads,$id['id'])); //выводим пользователю результат (рендерим страницу)
 	}
 
-	//метод работающий при гет запросе - удалить запись
+	/** метод работающий при гет запросе - удалить запись */
 	function delAction(){
-
-		$fc=FrontController::getInstance();
-		$delId = $fc->getParams();//получаем id
-		$ads = new AdsController(); //вызываем класс по работе с объявлениями
-		$ads->deleteAds($delId['id']); //удаляем запись из базы
-		$main = StorageController::getInstance(); //подключаемся к хранилищу
-		$ads = $main->getAdsFromBD(); //получаем данные объявлений из хранилища
-		$fc->setBody($this->ViewModels->render($ads)); //выводим пользователю результат (рендерим страницу)
-
+		$id = $this->fc->getParams();//получаем id редактируемой записи
+		$this->Models->deleteBD($id['id']); //удаляем запись из базы данных
+		$this->indexAction(); //продолжаем отображение данных и страницы
 	}
 
 }
